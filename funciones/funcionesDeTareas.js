e7d843af9a67a8341dc13c8003f38435f3993acf
Richard Cuevas <richardcuevas2018@gmail.com>
2022-05-31 15:55:05 -0300

    agregando const a funcionesExt para destructuring

--- funciones/funcionesDeTareas.js
+++ funciones/funcionesDeTareas.js
@@ -7,34 +7,34 @@ const listar= function(){
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         console.log(`(${tareas.id})Titulo: ${tareas.titulo} ---- Estado: ${tareas.estado} ---- Fecha: ${tareas.fecha}`);
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    })};
+})};
 const agregar= function(tareaNueva){
-        tareas.push(tareaNueva);
-        console.log("Tarea agregada con exito.");
-        escribir(tareas);
-        setTimeout(listar, 1000);
-    };
+    tareas.push(tareaNueva);
+    console.log("Tarea agregada con exito.");
+    escribir(tareas);
+    setTimeout(listar, 1000);
+};
 const eliminar= function(id){
-        tareas.splice(id -1, 1)//splice elimina el objeto con el parametro de la id - 1, ya que en array empieza desde 0.
-        actId(tareas);
-        escribir(tareas);
-        setTimeout(listar, 1000);
-    };
+    tareas.splice(id -1, 1)//splice elimina el objeto con el parametro de la id - 1, ya que en array empieza desde 0.
+    actId(tareas);
+    escribir(tareas);
+    setTimeout(listar, 1000);
+};
 const actualizar= function(titulo, estado, id){
-        tareas[tareas.findIndex(obj => obj.id === id)].titulo = titulo;//findIndex busca un elemento en tareas.id igual a la id ingresada. Una vez hecho, modifica los valores de la tarea correspondiente.
-        tareas[tareas.findIndex(obj => obj.id === id)].estado = estado;
-        tareas[tareas.findIndex(obj => obj.id === id)].fecha = fechaDeCreacion();
-        escribir(tareas);
-        setTimeout(listar, 1000);
-    };
+    tareas[tareas.findIndex(obj => obj.id === id)].titulo = titulo;//findIndex busca un elemento en tareas.id igual a la id ingresada. Una vez hecho, modifica los valores de la tarea correspondiente.
+    tareas[tareas.findIndex(obj => obj.id === id)].estado = estado;
+    tareas[tareas.findIndex(obj => obj.id === id)].fecha = fechaDeCreacion();
+    escribir(tareas);
+    setTimeout(listar, 1000);
+};
 const filtrar= function(estado,array){
-        let filtrados = array.filter(array => array.estado === estado);//filter busca el estado en el array, los dos pasados como parametros, devuelve otro array con los datos buscados, sino array vacio, sin modificar el anterior.
-        filtrados.forEach(tareas => {//forEach recorre el array uno por uno.
-            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            console.log(`(${tareas.id})Titulo: ${tareas.titulo} ---- Estado: ${tareas.estado} ---- Fecha: ${tareas.fecha}`);
-            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        })
-    };
+    let filtrados = array.filter(array => array.estado === estado);//filter busca el estado en el array, los dos pasados como parametros, devuelve otro array con los datos buscados, sino array vacio, sin modificar el anterior.
+    filtrados.forEach(tareas => {//forEach recorre el array uno por uno.
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(`(${tareas.id})Titulo: ${tareas.titulo} ---- Estado: ${tareas.estado} ---- Fecha: ${tareas.fecha}`);
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    })
+};
 
 module.exports = {
     listar,
--- funciones/funcionesExternas.js
+++ funciones/funcionesExternas.js
@@ -1,16 +1,22 @@
 const {readFileSync, writeFileSync} = require("fs");
 const tareas = JSON.parse(readFileSync("../baseDeDatos/tareas.json", "utf-8"));//// convierte el formato JSON a formato de js para poder trabajar con el.
 let fecha = new Date();
-module.exports = {
-    escribir: function(tareas){
-        writeFileSync("../baseDeDatos/tareas.json", JSON.stringify(tareas, null, 4));//escribe el nuevo formato js y lo convierte a json, para guardarlo en la base de datos.
-    },
-    actId: function(tareas){ //modifica las id empezando desde 1, y las siguienes ++.
-        for(x = 0; x < tareas.length; x++){
+
+const escribir= function(tareas){
+    writeFileSync("../baseDeDatos/tareas.json", JSON.stringify(tareas, null, 4));//escribe el nuevo formato js y lo convierte a json, para guardarlo en la base de datos.
+};
+const actId= function(tareas){ //modifica las id empezando desde 1, y las siguienes ++.
+    for(x = 0; x < tareas.length; x++){
             x === 0? tareas[x].id = 1 : tareas[x].id = x + 1;}
-    },
-    fechaDeCreacion: function(){
-        return fecha.toLocaleString();//asigna la fecha y hora del momento.
-        //`${fecha.getDate()}/${fecha.getMonth() +1}/${fecha.getFullYear()} [${fecha.getHours()}:${fecha.getMinutes()}]`
-    }
+};
+const fechaDeCreacion= function(){
+    return fecha.toLocaleString();//asigna la fecha y hora del momento.
+    //`${fecha.getDate()}/${fecha.getMonth() +1}/${fecha.getFullYear()} [${fecha.getHours()}:${fecha.getMinutes()}]`
 }
+
+
+module.exports ={
+    escribir,
+    actId,
+    fechaDeCreacion,
+};
