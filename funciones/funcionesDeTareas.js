c68d045324573ac80444549bd281dd2cb512314f
Richona <105828822+Richona@users.noreply.github.com>
2022-06-06 23:24:06 -0300

    agregue la funcion leerJson y la aplique a todas las funciones

--- funciones/funcionesDeTareas.js
+++ funciones/funcionesDeTareas.js
@@ -1,26 +1,31 @@
 const {readFileSync} = require("fs");
-let tareas = JSON.parse(readFileSync("../baseDeDatos/tareas.json", "utf-8"));//// convierte el formato JSON a formato de js para poder trabajar con el.
 const {escribir,actId,fechaDeCreacion} = require("./funcionesExternas");
 
+const leerJson = function(){
+    return JSON.parse(readFileSync("../baseDeDatos/tareas.json", "utf-8"));//// convierte el formato JSON a formato de js para poder trabajar con el.
+}
 const listar= function(){
-    tareas.forEach(tareas => {//forEach recorre el array uno por uno.
+    leerJson().forEach(tareas => {//forEach recorre el array uno por uno.
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         console.log(`(${tareas.id})Titulo: ${tareas.titulo} ---- Estado: ${tareas.estado} ---- Fecha: ${tareas.fecha}`);
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 })};
 const agregar= function(tareaNueva){
+    let tareas = leerJson();
     tareas.push({id :tareas[tareas.length - 1].id +1, ...tareaNueva, fecha: fechaDeCreacion()});
     console.log("Tarea agregada con exito.");
     escribir(tareas);
     setTimeout(listar, 1000);
 };
 const eliminar= function(id){
+    let tareas = leerJson();
     tareas.splice(id -1, 1)//splice elimina el objeto con el parametro de la id - 1, ya que en array empieza desde 0.
     actId(tareas);
     escribir(tareas);
     setTimeout(listar, 1000);
 };
 const actualizar= function(titulo, estado, id){
+    let tareas = leerJson();
     tareas.findIndex(obj => obj.id === id) === -1 && "Id no encontrada, ingrese una correcta por favor";//comprueba si la id es la correcta.
     tareas[tareas.findIndex(obj => obj.id === id)].titulo = titulo;//findIndex busca un elemento en tareas.id igual a la id ingresada. Una vez hecho, modifica los valores de la tarea correspondiente.
     tareas[tareas.findIndex(obj => obj.id === id)].estado = estado;
@@ -29,6 +34,7 @@ const actualizar= function(titulo, estado, id){
     setTimeout(listar, 1000);
 };
 const filtrar= function(estado){
+    let tareas = leerJson();
     let filtrados = tareas.filter(array => array.estado === estado);//filter busca el estado en el array, los dos pasados como parametros, devuelve otro array con los datos buscados, sino array vacio, sin modificar el anterior.
     filtrados.forEach(tareas => {//forEach recorre el array uno por uno.
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
